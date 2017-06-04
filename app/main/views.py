1e837bd89436f0f00174e3688be32db8db1fb853
Scott McCormack <scott.mccormack@outlook.com>
2017-06-04 12:01:34 +0800

    Views added for logging a user out and creating new transactions. Changes made to the blockchain-log view.

--- app/main/views.py
+++ app/main/views.py
@@ -1,9 +1,8 @@
 from flask import render_template, session, redirect, url_for, current_app, flash
-from datetime import datetime
 from .. import mongo
 from ..models import User, Miner
 from . import main
-from .forms import LoginForm, RegistrationForm
+from .forms import LoginForm, RegistrationForm, TransactionForm
 from .table import BlockchainTable
 
 
@@ -25,12 +24,14 @@ def index():
             session['known'] = True
             session['name'] = username
 
-            return render_template('index.html', form=False,
+            return render_template('index.html',
+                                   form=False,
                                    name=session.get('name'),
                                    known=session.get('known', False))
 
     return render_template('index.html',
-                           form=form, name=session.get('name'),
+                           form=form,
+                           name=session.get('name'),
                            known=session.get('known', False))
 
 
@@ -48,40 +49,19 @@ def register():
 
         # Attempt to register a new user
         registration_success, response = user.register_new_user()
+        flash(response)
 
         # If registration was successful, initialise the user and give them some coins
         if registration_success:
             miner = Miner()
-            miner.initialise_new_user(user, datetime.now())
+            miner.initialise_new_user(user)
 
-        flash(response)
+            # Log out any users and send them to the homepage to log in
+            return redirect(url_for('.logout'))
 
     return render_template('register.html',
-                           form=form)
-
-
-@main.route('/blockchain-log', methods=['GET', 'POST'])
-def blockchain_log():
-    # Insert some test table entries
-    blockchain_transactions = mongo.db.blockchain.find()
-    table_list = []
-
-    for transaction in blockchain_transactions:
-        to_user = mongo.db.users.find_one({'public_key': transaction['to_user_pk']})
-
-        table_list.append({
-            'from_user': '<miner>',
-            'to_user': to_user['username'],
-            'amount': transaction['cc_amount'],
-            'timestamp': transaction['timestamp'],
-            'nonce': transaction['nonce'],
-            'miner_verify': transaction['miner_verify']
-        })
-
-    table = BlockchainTable(table_list)
-
-    return render_template('blockchain-log.html',
-                           table=table)
+                           form=form,
+                           known=session.get('known', False))
 
 
 @main.route('/logout', methods=['GET'])
@@ -89,3 +69,87 @@ def logout():
     session['known'] = False
     session['name'] = False
     return redirect(url_for('.index'))
+
+
+@main.route('/blockchain-log', methods=['GET'])
+def blockchain_log():
+    # Get all the blockchain transactions from the database 'blockchain'
+    blockchain_transactions = mongo.db.blockchain.find()
+    # Parse transactions and load them into 'table_list'
+    table_list = []
+
+    for transaction in blockchain_transactions:
+        to_user_pk = transaction['to_user_pk']
+        from_user_pk = transaction['from_user_pk']
+
+        # Helper function to encode as 'UTF-8' if not:
+        if isinstance(to_user_pk, str):
+            to_user_pk = to_user_pk.encode('UTF-8')
+        if isinstance(from_user_pk, str):
+            from_user_pk = from_user_pk.encode('UTF-8')
+
+        # Cross-reference user database to get actual username
+        to_user = mongo.db.users.find_one(
+            {'public_key': to_user_pk}
+        )
+        to_user_username = to_user['username']
+
+        # If from 'miner' set from username as simply 'miner'
+        if from_user_pk == b'<miner>':
+            from_user_username = '<miner>'
+        else:
+            from_user = mongo.db.users.find_one(
+                {'public_key': from_user_pk}
+            )
+            from_user_username = from_user['username']
+
+        if to_user is not None:
+            table_list.append({
+                'from_user': from_user_username,
+                'to_user': to_user_username,
+                'amount': transaction['cc_amount'],
+                'timestamp': transaction['timestamp'],
+                'nonce': transaction['nonce'],
+                'miner_verify': transaction['miner_verify']
+            })
+
+    table = BlockchainTable(table_list)
+
+    return render_template('blockchain-log.html',
+                           table=table,
+                           known=session.get('known', False))
+
+
+@main.route('/transactions', methods=['GET', 'POST'])
+def transactions():
+    # Determine if a user is currently logged in
+    user_known = session.get('known', False)
+    user_name = session.get('name', False)
+
+    # If not redirect them back to the homepage
+    if not user_known:
+        flash('Please log in')
+        return redirect(url_for('.index'))
+
+    # Initialise the Transaction Form
+    form = TransactionForm()
+
+    # Get a list of users who we can send chriscoins to, excluding self
+    username_list = User.get_username_list(username=user_name)
+    form.to_user.choices = username_list
+
+    if form.validate_on_submit():
+        from_username = session['name']
+        to_username = form.to_user.data
+        amount = form.amount.data
+
+        response = User.create_new_transaction(
+            from_username=from_username,
+            to_username=to_username,
+            amount=amount
+        )
+
+        flash(response)
+
+    return render_template('transactions.html', form=form,
+                           known=user_known)
